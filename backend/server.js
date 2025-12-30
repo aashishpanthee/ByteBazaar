ac8c3470f083b5f03718915a73d85ba6de94ea2d
Aashish Panthee <aashishpanthee060@gmail.com>
2025-12-31 00:02:07 +0545

    update: change port no. of backend

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,15 @@
-import express from "express";
-import dotenv from "dotenv";
 import colors from "colors";
 import cookieParser from "cookie-parser";
-dotenv.config();
+import dotenv from "dotenv";
+import express from "express";
 import connectDB from "./config/db.js";
+import orderRoute from "./routes/orderRoute.js";
 import productRoute from "./routes/productRoute.js";
 import userRoute from "./routes/userRoute.js";
-import orderRoute from "./routes/orderRoute.js";
-const port = process.env.PORT || 5000;
+dotenv.config();
+const port = process.env.PORT || 5001;
 
-import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
+import { errorHandler, notFound } from "./middleware/errorMiddleware.js";
 connectDB(); //Connection to MONGODB
 
 const app = express();
@@ -22,7 +22,6 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 app.get("/", (req, res) => {
-  s;
   res.send(`Server is ready`);
 });
 
